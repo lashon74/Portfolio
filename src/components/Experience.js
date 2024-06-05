33f6eeba639fc34b73eb6cb347ad9c1d4991fa6c
Lashon Hudson <lashonhud@gmail.com>
2024-06-04 21:44:02 -0400

    first commit

---
+++ src/components/Experience.js
@@ -0,0 +1,116 @@
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import LiIcon from "./LiIcon";
+
+const Details = ({
+  position,
+  company,
+  companyLink,
+  time,
+  address,
+  bullet1,
+  bullet2,
+  bullet3,
+  bullet4,
+  bullet5,
+}) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
+    >
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {position}{" "}
+          <a
+            className="capitalize text-primary dark:text-primaryDark"
+            href={companyLink}
+            target={"_blank"}
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+          {time} | {address}
+        </span>
+        <ul>
+          <li className="font-medium w-full md:text-sm">{bullet1}</li>
+          <li className="font-medium w-full md:text-sm">{bullet2}</li>
+          <li className="font-medium w-full md:text-sm">{bullet3}</li>
+          <li className="font-medium w-full md:text-sm">{bullet4}</li>
+          <li className="font-medium w-full md:text-sm">{bullet5}</li>
+        </ul>
+        {/* <p className="font-medium w-full md:text-sm"> ● {bullet1}</p> */}
+      </motion.div>
+    </li>
+  );
+};
+
+const Experience = () => {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
+  return (
+    <div className="my-64">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Experience
+      </h2>
+
+      <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
+        <motion.div
+          className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark 
+            origin-top  dark:bg-primaryDark dark:shadow-3xl"
+          style={{ scaleY: scrollYProgress }}
+        />
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+          <Details
+            position="Software Engineer II"
+            company="Integrisource"
+            companyLink="https://integrisource.net/"
+            time="2023-Present"
+            address="Remote, USA"
+            bullet1="● Implemented security protocols for applications, resulting in a 40% reduction in security breaches and an overall improvement in customer trust and satisfaction."
+            bullet2="● Designed and developed a scalable .NET application using C#, ASP.NET, and MVC, resulting in a 40% increase in system efficiency and a 20% reduction in operational costs."
+            bullet3="● Converting a local client-based school board system to a data-centric web applications using ASP.NET, C#.NET, and Services with Agile/SCRUM methodology that the Michigan Wayne County school board will adopt."
+            bullet4="● Work closely with business analysis to optimize data driven web applications that will be used across multiple school districts, test changes made on client and server side to improve speed and efficiency."
+            bullet5="● Implement business logic in C#.NET, business intelligence/ETL solutions, and business rules to integrate disparate data sources toward a centralized data store."
+          />
+
+          <Details
+            position="Software Engineer I"
+            company="Precision Castparts Corporation (PCC) Airfoils"
+            companyLink="https://www.pccairfoils.com/business-units/douglas/"
+            time="2021 - 2023"
+            address="Douglas, GA."
+            bullet1="● Converted legacy applications written in VB6 to C# .NET and enhanced Stored Procedures, Triggers, and Tables which increased productivity by 150% resulting in a 20% reduction in operation costs."
+            bullet2="● Developed and maintained databases and procedures using SQL Server, resulting in a 30% increase in system reliability and a 15% reduction in data-related issues."
+            bullet3="● Collaborated with other developers and stakeholders to create and maintain unit tests for applications, resulting in a 25% reduction in bug-related delays and an overall improvement in code quality."
+            bullet4="● Set up and maintained a relational database system for storage, retrieval, and manipulation of data, leading to improved data accessibility."
+            bullet5="● Debugged and troubleshot code, resulting in a 30% reduction in production issues and a 20% increase in customer satisfaction."
+          />
+
+          <Details
+            position="Freelance Software Developer"
+            company="Hudson Web Design & Consulting"
+            companyLink="#"
+            time="2017-Present"
+            address="Savannah, GA."
+            bullet1="● Develop interactive websites and landing pages for local businesses that enhance traffic, page views, and user experience resulting in the client receiving more revenue."
+          />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/components/Skills.js
@@ -0,0 +1,73 @@
+import { motion } from "framer-motion";
+import React, { useRef } from "react";
+
+const Skill = ({ image, name, x, y }) => {
+  const ref = useRef(null);
+  return (
+    <motion.div
+      ref={ref}
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
+      viewport={{ once: true }}
+      className="cursor-pointer w-max origin-center absolute 
+       font-semibold bg-dark text-light py-3 px-6 rounded-full dark:bg-light dark:text-dark
+       lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3  xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold
+       "
+    >
+      {name}
+      {image}
+    </motion.div>
+  );
+};
+
+const Skills = () => {
+  const ref = useRef(null);
+  return (
+    <>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
+        Skills
+      </h2>
+      <div
+        ref={ref}
+        className="w-full h-[100vh] relative bg-circularLight dark:bg-circularDark  flex items-center justify-center 
+      mb-64 md:mb-32 rounded-full
+      lg:bg-circularLightLg lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd 
+      sm:bg-circularLightSm sm:dark:bg-circularDarkSm lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
+      "
+      >
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          className="cursor-pointer flex rounded-full font-semibold bg-dark text-light p-8 shadow-dark
+        dark:bg-light dark:text-dark lg:p-6 md:p-4 xs:text-xs xs:p-2
+        "
+        >
+          Web
+        </motion.div>
+
+        <Skill name="HTML" x="-20vw" y="2vw" />
+        <Skill name="TFS" x="-10vw" y="2vw" />
+        <Skill name="TortoiseSVN" x="10vw" y="1vw" />
+        <Skill name="JIRA" x="-15vw" y="11vw" />
+        <Skill name="CSS" x="-5vw" y="-10vw" />
+        <Skill name="JavaScript" x="20vw" y="6vw" />
+        <Skill name="ReactJS" x="0vw" y="12vw" />
+        <Skill name="Reporting" x="0vw" y="20vw" />
+        <Skill name="c#" x="-20vw" y="-16vw" />
+        <Skill name="Scrum" x="-20vw" y="-9vw" />
+        <Skill name="ASP.Net Core" x="15vw" y="-12vw" />
+        <Skill name="S.O.L.I.D" x="15vw" y="-8vw" />
+        <Skill name="Github" x="-35vw" y="-5vw" />
+        <Skill name="Web Design" x="32vw" y="-5vw" />
+        <Skill name="SQL" x="0vw" y="-20vw" />
+        <Skill name="PostMan" x="-5vw" y="-17vw" />
+        <Skill name="JQuery" x="-25vw" y="18vw" />
+        <Skill name="VB .Net" x="-35vw" y="17vw" />
+        <Skill name="Bootstrap" x="28vw" y="17vw" />
+        <Skill name="Rest API" x="18vw" y="20vw" />
+      </div>
+    </>
+  );
+};
+
+export default Skills;
