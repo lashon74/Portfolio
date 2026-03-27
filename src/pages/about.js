41b847f79c423c9e7e4804df5d2ddfb074614081
Lashon Hudson <lashonhud@gmail.com>
2026-03-27 17:13:23 -0400

    fixed logo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
-import profile from "../../public/images/profile/logo.JPG";
+import profile from "../../public/images/profile/logo.jpg";
 import { useInView, useMotionValue, useSpring, motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import Skills from "@/components/Skills";
